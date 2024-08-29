6b26d41946da34a9444952d439b2a428282cf78f
Sahil Bera <144260776+Sahil-1010@users.noreply.github.com>
2024-08-29 18:04:00 +0530

    Add files via upload
    done

---
+++ app.js
@@ -0,0 +1,5 @@
+// Add event listener to the Consult Now button
+document.querySelector('.hero button').addEventListener('click', function() {
+    console.log('Consult Now button clicked!');
+    // Add your logic here to handle the button click event
+});
