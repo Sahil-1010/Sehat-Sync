c8826d13ff1455948a0901e0b193280b454adad9
Sahil Bera <144260776+Sahil-1010@users.noreply.github.com>
2024-09-01 00:57:24 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-// Add event listener to the Consult Now button
-document.querySelector('.hero button').addEventListener('click', function() {
-    console.log('Consult Now button clicked!');
-    // Add your logic here to handle the button click event
-});
